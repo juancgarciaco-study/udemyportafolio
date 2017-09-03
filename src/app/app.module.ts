1c237220b185c7d232dab11c901367076dd9535e
Juan Carlos Garcia <juancgarciaco@gmail.com>
2017-09-03 08:58:40 -0500

    Producto added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeMainComponent } from './components/home-main/home-main.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProductoComponent } from './components/producto/producto.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { AboutComponent } from './components/about/about.component';
     FooterComponent,
     HomeMainComponent,
     AboutComponent,
+    ProductoComponent,
   ],
   imports: [
     BrowserModule
