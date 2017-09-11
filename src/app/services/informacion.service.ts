343e8a924594907a58e4c8463f8b9bf769b6de9b
Juan Carlos Garcia <juancgarciaco@gmail.com>
2017-09-10 21:22:31 -0500

    Se agrega animate.css se modifica un nombre en los componentes se reorganiza informacion service se agregan datos del team work a base de datos firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -3,29 +3,47 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class InformacionService {
-  pageinfo: object = {
+  info_main_headerfooter_loaded: boolean = false;
+  info_main_headerfooter: any = {
     'basic': {
-        'title': '',
-        'email': '',
-        'authorName': '',
-        'authorWeb': ''
+      'title': '',
+      'email': '',
+      'authorName': '',
+      'authorWeb': ''
     },
     'social': {
-        'facebook': '',
-        'twitter': '',
-        'instagram': ''
-    },
-    'teamWork': []
-};
+      'facebook': '',
+      'twitter': '',
+      'instagram': ''
+    }
+  };
+
+  info_aboutus_teamwork_loaded: boolean = false;
+  info_aboutus_teamwork: any[] = [];
 
   constructor(
     public http: Http
   ) {
+    this.loadDataHeaderFooter();
+    this.loadDataAboutUs_TeamWork();
+  }
+
+  public loadDataHeaderFooter() {
     this.http.get('assets/data/info.pages.json')
       .subscribe(data => {
-        this.pageinfo = data.json();
-        console.log(this.pageinfo);
+        this.info_main_headerfooter = data.json();
+        this.info_main_headerfooter_loaded = true;
+        // console.log(this.info_main_headerfooter);
       });
   }
 
+  public loadDataAboutUs_TeamWork() {
+    this.http.get('https://jcgco-udemy-html2angular.firebaseio.com/teamWork.json')
+      .subscribe(data => {
+        this.info_aboutus_teamwork = data.json();
+        this.info_aboutus_teamwork_loaded = true;
+        // console.log(this.info_main_headerfooter);
+      });
+
+  }
 }
