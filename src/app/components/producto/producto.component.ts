644677d9d3dd74dd0faffc9c1cf729bb940eb31c
Juan Carlos Garcia <juancgarciaco@gmail.com>
2017-09-20 22:56:53 -0500

    Completar pagina de producto con imagenes

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-producto',
@@ -7,17 +8,30 @@ import { ActivatedRoute } from '@angular/router';
   styles: []
 })
 export class ProductoComponent implements OnInit {
+  product: any = undefined;
+  cod: string = undefined;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private _ps: ProductosService
   ) {
     route.params.subscribe(params => {
-      console.log(params);
-      console.log(params['id']);
+      if (params['id']) {
+        this.cod = params['id'];
+        console.log(params);
+        console.log(this.cod);
+
+        // this.product = _ps.cargar_producto2(id)[0];
+        // console.log(this.product);
+
+        _ps.cargar_producto(this.cod).subscribe(res => {
+          this.product = res.json();
+          console.log(this.product);
+        });
+      }
     });
   }
 
   ngOnInit() {
   }
-
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -7,10 +7,26 @@ export class ProductosService {
   productos_loaded: boolean = false;
   productos: any[] = [];
 
+  producto_loaded: boolean = false;
+
   constructor(
     private http: Http
   ) {
     this.cargar_productos();
+  }
+
+  public cargar_producto2(prodId: string) {
+    return this.http.get(`https://jcgco-udemy-html2angular.firebaseio.com/productos/${prodId}.json`)
+      .subscribe(data => {
+        const _data = data.json();
+        this.producto_loaded = true;
+        return _data;
+        // console.log(this.productos);
+      });
+  }
+
+  public cargar_producto(prodId: string) {
+    return this.http.get(`https://jcgco-udemy-html2angular.firebaseio.com/productos/${prodId}.json`);
   }
 
   public cargar_productos() {
@@ -18,10 +34,10 @@ export class ProductosService {
     if (this.productos.length === 0) {
       this.http.get('https://jcgco-udemy-html2angular.firebaseio.com/productos_idx.json')
         .subscribe(data => {
-          setTimeout( () => {
+          setTimeout(() => {
             this.productos = data.json();
             this.productos_loaded = true;
-          }, 2000);
+          }, 1000);
           // console.log(this.productos);
         });
     }
